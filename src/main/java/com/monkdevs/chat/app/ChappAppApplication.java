5007bb7680029fcc91d7711df07859c6f7cf0611
Shubham Gaigawale <shubhamgawale1999@outlook.com>
2020-07-31 15:06:13 +0530

    first commit

---
+++ src/main/java/com/monkdevs/chat/app/ChappAppApplication.java
@@ -0,0 +1,13 @@
+package com.monkdevs.chat.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChappAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChappAppApplication.class, args);
+	}
+
+}
